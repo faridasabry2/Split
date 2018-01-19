214d0f16d2d4364631caf88d19a7c85c4c8a8d4a
Angelica Luna <aluna@smith.edu>
2018-01-19 10:29:44 -0500

    Add & delete to activity

--- app/src/main/java/com/smith/split/ItemSplit.java
+++ app/src/main/java/com/smith/split/ItemSplit.java
@@ -1,25 +1,21 @@
 package com.smith.split;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toolbar;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ItemSplit extends AppCompatActivity {
-    ArrayList<String> itemList;
+    ArrayList<String> nameList;
     ArrayAdapter<String> adapter;
-    EditText itemText;
+    EditText nameText;
     Button addButton;
     ListView lview;
 
@@ -28,24 +24,47 @@ public class ItemSplit extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_split);
 
-        lview = findViewById(R.id.listView);
-        itemText = findViewById(R.id.editText);
+        lview = (ListView)findViewById(R.id.listView);
+        nameText = findViewById(R.id.editText);
         addButton = (Button) findViewById(R.id.addButton);
 
-        itemList = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,itemList);
+        nameList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,nameList);
 
         View.OnClickListener addListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                itemList.add(itemText.getText().toString());
-                itemText.setText("");
+                nameList.add(nameText.getText().toString());
+                nameText.setText("");
                 adapter.notifyDataSetChanged();
             }
         };
+   //     addButton.setOnClickListener(addListener);
+
+ //       lview.setAdapter(adapter);
+        lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                SparseBooleanArray checkposition = lview.getCheckedItemPositions();
+                int count = lview.getCount();
+
+                for(int item = count-1; item >=0; item--){
+                    if (checkposition.get(item)){
+                        adapter.remove(nameList.get(item));
+                    }
+                }
+                checkposition.clear();
+                adapter.notifyDataSetChanged();
+
+               // return false;
+            }
+        });
         addButton.setOnClickListener(addListener);
 
         lview.setAdapter(adapter);
     }
+
 }
 
