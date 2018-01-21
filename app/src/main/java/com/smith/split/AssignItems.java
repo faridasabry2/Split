d54fee05f28695f56423676f263aa02fba297c19
Farida Sabry <fsabry@smith.edu>
2018-01-21 06:45:33 -0500

    modified list view UI so adding and deleting items is a lot more intuitive

---
+++ app/src/main/java/com/smith/split/AssignItems.java
@@ -0,0 +1,124 @@
+package com.smith.split;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AssignItems extends AppCompatActivity {
+
+    ArrayList<String> itemList;
+    ArrayList<String> priceList;
+
+    ArrayAdapter<String> itemAdapter;
+    ArrayAdapter<String> priceAdapter;
+
+    ListView itemsView;
+    EditText itemName;
+    EditText itemPrice;
+    Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assign_items);
+
+        itemsView = (ListView)findViewById(R.id.listView);
+        addButton = (Button) findViewById(R.id.addItemButton);
+
+        itemName = (EditText) findViewById(R.id.itemEditText);
+        itemPrice = (EditText) findViewById(R.id.priceEditText);
+
+        itemName.addTextChangedListener(watcher);
+        itemPrice.addTextChangedListener(watcher);
+
+        itemList = new ArrayList<>();
+        priceList = new ArrayList<>();
+
+        loadItemList();
+
+        // ADD ITEM WHEN button is clicked
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                itemList.add(itemName.getText().toString());
+                itemName.setText("");
+                itemAdapter.notifyDataSetChanged();
+
+                priceList.add(itemPrice.getText().toString());
+                itemPrice.setText("");
+                priceAdapter.notifyDataSetChanged();
+
+                Log.i("whatevs", itemList.toString() + " -- " + priceList.toString());
+            }
+        });
+    }
+
+    private final TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        { }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {}
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (isEmpty(itemName) || isEmpty(itemPrice)) {
+                addButton.setEnabled(false);
+            } else {
+                addButton.setEnabled(true);
+            }
+        }
+    };
+
+    private boolean isEmpty(EditText editText) {
+        return editText.getText().toString().trim().length() == 0;
+    }
+
+    private void loadItemList() {
+        if (itemAdapter == null) {
+            itemAdapter = new ArrayAdapter<String>(this, R.layout.item_row, R.id.item_name, itemList);
+            itemsView.setAdapter(itemAdapter);
+        } else {
+            itemAdapter.clear();
+            itemAdapter.addAll(itemList);
+            itemAdapter.notifyDataSetChanged();
+        }
+
+        if(priceAdapter == null) {
+            priceAdapter = new ArrayAdapter<String>(this, R.layout.item_row, R.id.item_price, priceList);
+            itemsView.setAdapter(priceAdapter);
+        } else {
+            priceAdapter.clear();
+            priceAdapter.addAll(priceList);
+            priceAdapter.notifyDataSetChanged();
+        }
+    }
+
+    public void deleteEntry(View view) {
+        View parent = (View) view.getParent();
+
+        TextView itemTextView = (TextView) findViewById(R.id.item_name);
+        TextView priceTextView = (TextView) findViewById(R.id.item_price);
+
+        String item = String.valueOf(itemTextView.getText());
+        String price = String.valueOf(priceTextView.getText());
+
+        itemList.remove(item);
+        priceList.remove(price);
+
+        itemAdapter.notifyDataSetChanged();
+        priceAdapter.notifyDataSetChanged();
+    }
+
+}
+
+
--- app/src/main/java/com/smith/split/ItemSplit.java
+++ app/src/main/java/com/smith/split/ItemSplit.java
@@ -1,70 +1,91 @@
 package com.smith.split;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
 public class ItemSplit extends AppCompatActivity {
     ArrayList<String> nameList;
     ArrayAdapter<String> adapter;
+    ListView namesView;
     EditText nameText;
     Button addButton;
-    ListView lview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_split);
 
-        lview = (ListView)findViewById(R.id.listView);
-        nameText = findViewById(R.id.editText);
-        addButton = (Button) findViewById(R.id.addButton);
+        namesView = (ListView)findViewById(R.id.listView);
+        addButton = (Button) findViewById(R.id.addItemButton);
+        nameText = (EditText) findViewById(R.id.nameEditText);
 
         nameList = new ArrayList<>();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,nameList);
+        loadNameList();
 
-        View.OnClickListener addListener = new View.OnClickListener() {
-            @Override
+        // ADD ITEM WHEN button is clicked
+        addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 nameList.add(nameText.getText().toString());
                 nameText.setText("");
                 adapter.notifyDataSetChanged();
             }
-        };
-   //     addButton.setOnClickListener(addListener);
+        });
 
- //       lview.setAdapter(adapter);
-        lview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // SWITCH TO CAMERA VIEW TO SCAN
+        Button scanButton = (Button) findViewById(R.id.scanButton);
 
-
-                SparseBooleanArray checkposition = lview.getCheckedItemPositions();
-                int count = lview.getCount();
-
-                for(int item = count-1; item >=0; item--){
-                    if (checkposition.get(item)){
-                        adapter.remove(nameList.get(item));
-                    }
-                }
-                checkposition.clear();
-                adapter.notifyDataSetChanged();
-
-               // return false;
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context packageContext = getApplicationContext();
+                Intent intent = new Intent(packageContext, CameraCapture.class);
+                startActivity(intent);
             }
         });
-        addButton.setOnClickListener(addListener);
 
-        lview.setAdapter(adapter);
+        // SWITCH TO MANUALLY ENTER ITEMS
+        Button enterItemsButton = (Button) findViewById(R.id.enterItemsButton);
+
+        enterItemsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context packageContext = getApplicationContext();
+                Intent intent = new Intent(packageContext, AssignItems.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void loadNameList() {
+        if (adapter == null) {
+            adapter = new ArrayAdapter<>(this, R.layout.row, R.id.person_name, nameList);
+            namesView.setAdapter(adapter);
+        } else {
+            adapter.clear();
+            adapter.addAll(nameList);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
+    public void deleteEntry(View view) {
+        View parent = (View) view.getParent();
+        TextView personTextView = (TextView) findViewById(R.id.person_name);
+        String person = String.valueOf(personTextView.getText());
+        nameList.remove(person);
+        adapter.notifyDataSetChanged();
+
+//        loadNameList();
     }
 
 }
